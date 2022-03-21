57ca1fb878317d3e3ef5d8a1961be4d11e615f5f
Kay <77486531+HelloItsKay@users.noreply.github.com>
2022-03-21 19:30:14 +0200

    Clean up Startup and Project cs

---
+++ ASP.NET-Core-OnlineShop/Startup.cs
@@ -0,0 +1,66 @@
+using ASP.NET_Core_OnlineShop.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ASP.NET_Core_OnlineShop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddDbContext<ApplicationDbContext>(options => options
+                .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services
+                .AddDatabaseDeveloperPageExceptionFilter();
+
+            services
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services
+                .AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app
+                    .UseDeveloperExceptionPage() 
+                    .UseMigrationsEndPoint();
+            }
+            else
+            {
+                app
+                    .UseExceptionHandler("/Home/Error")
+                    .UseHsts();
+            }
+            app
+                .UseHttpsRedirection()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints => {
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
+                  });
+        }
+    }
+}
