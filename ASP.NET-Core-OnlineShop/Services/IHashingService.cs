c03b9c574c79c30c25328cfeeed69f18942f412b
Kay <77486531+HelloItsKay@users.noreply.github.com>
2022-04-21 13:41:25 +0300

    I didnt save previous the change.... now it works

---
+++ ASP.NET-Core-OnlineShop/Services/IHashingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_OnlineShop.Services
+{
+    public interface IHashingService
+    {
+        public string Encrypt(string clearText);
+        public string Decrypt(string cipherText);
+    }
+}
